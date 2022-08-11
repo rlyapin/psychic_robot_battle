cac3b004833f5b37987752e942cb5b54040c3758
rlyapin <lyapin.rk@gmail.com>
2022-08-11 18:03:21 +0900

    Moved all postgres parameters to a single secret

--- frontend/src/lib/db.js
+++ frontend/src/lib/db.js
@@ -1,16 +1,12 @@
 // https://www.reddit.com/r/sveltejs/comments/v1d1ms/connecting_my_database_with_sveltekit/
 import pg from "pg";
 
-const db = process.env.POSTGRES_DB;
-const user = process.env.POSTGRES_USER;
-const pwd = process.env.POSTGRES_PASSWORD;
-
 const pool = new pg.Pool({
-  database: db,
-  user: user,
-  password: pwd,
-  host: "timescaledb.default.svc.cluster.local",
-  port: 5432,
+  database: process.env.POSTGRES_PSYCHIC_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOSTNAME,
+  port: process.env.POSTGRES_PORT,
 });
 
 export const connectToDB = async () => await pool.connect();
--- timescaledb/init.sql
+++ timescaledb/init.sql
@@ -1,2 +1,6 @@
+CREATE DATABASE psychic_db;
+CREATE DATABASE airflow_db;
+
+\connect psychic_db;
 CREATE TABLE IF NOT EXISTS events
 (id SERIAL PRIMARY KEY, event_time timestamp, session_id VARCHAR(50), click VARCHAR(50), pred VARCHAR(50));
