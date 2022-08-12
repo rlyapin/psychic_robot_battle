1ae97b40c0022a5ffcdd6c280d2226b325b6d3bf
rlyapin <lyapin.rk@gmail.com>
2022-08-13 03:27:38 +0900

    Removed psychic deployment and adjusted psychic service name

--- frontend/src/routes/apis/predict.js
+++ frontend/src/routes/apis/predict.js
@@ -3,7 +3,8 @@ export async function GET({ url }) {
     console.log(history)
 
     // TODO: move info about model info to kubernetes configs and instead load environment variables
-    const api_endpoint = "http://psychic-pod.default-subdomain.default.svc.cluster.local:8080"
+    // const api_endpoint = "http://psychic-pod.default-subdomain.default.svc.cluster.local:8080"
+    const api_endpoint = "http://psychic:8080"
     const result = await (await fetch(`${api_endpoint}/predict/?history=${history}`, 
         {"method": "GET", "headers": {"content-type": "application/json", "accept": "application/json"}})).json();
     console.log(result)
