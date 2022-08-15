b03eabe3f3721b660d3e2363d7e4ae22a41f5632
rlyapin <lyapin.rk@gmail.com>
2022-08-16 04:01:29 +0900

    Added ingestion of click stats into psychic service at init

--- psychic/psychic/model.py
+++ psychic/psychic/model.py
@@ -1,8 +1,45 @@
+import logging
 import numpy as np
+import os
+import psycopg2
+
 
 class Psychic:
-	def __init__(self):
-		pass
+    def __init__(self):
+        self.prefix_stats = self.load_click_stats()
 
-	def predict(self, history):
-		return np.random.choice(["L", "R"])
+    def load_click_stats(self):
+        prefix_stats = {}
+
+        conn = psycopg2.connect(dbname=os.getenv("POSTGRES_PSYCHIC_DB"),
+                                user=os.getenv("POSTGRES_USER"),
+                                password=os.getenv("POSTGRES_PASSWORD"),
+                                host=os.getenv("POSTGRES_HOSTNAME"),
+                                port=os.getenv("POSTGRES_PORT"))
+
+        cursor = conn.cursor("click_stats_cursor")
+        cursor.execute("SELECT * FROM click_probs")
+
+        for record in cursor:
+            prefix, click, count = record
+            if prefix not in prefix_stats:
+                prefix_stats[prefix] = {}
+            prefix_stats[prefix][click] = count
+            logging.info(f"Ingested record: {record}")
+
+        conn.close()
+
+        return prefix_stats
+
+    def predict(self, history):
+        for i in range(len(history)):
+            prefix = history[i:]
+            if prefix in self.prefix_stats:
+                l_weight = self.prefix_stats[prefix].get("L", 0) + 1
+                r_weight = self.prefix_stats[prefix].get("R", 0) + 1
+                total_weight = l_weight + r_weight
+                probs = [l_weight / total_weight, r_weight / total_weight]
+                logging.info(f"Sampling for {prefix} prefix at {probs} probs")
+                return np.random.choice(["L", "R"], p=probs)
+
+        return np.random.choice(["L", "R"])
