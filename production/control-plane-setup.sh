a67d69eabc0b4bb7d063a6b48c535a8c9041cb02
rlyapin <lyapin.rk@gmail.com>
2022-09-05 00:57:52 +0900

    Moved bash restart after canal installation

--- production/control-plane-setup.sh
+++ production/control-plane-setup.sh
@@ -2,10 +2,11 @@
 
 # pod-network-cidr=10.244.0.0/16 is default for canal and hentzner
 kubeadm init --cri-socket="unix:///var/run/cri-dockerd.sock" --pod-network-cidr=10.244.0.0/16
-echo "export KUBECONFIG=/etc/kubernetes/admin.conf" > ~/.bashrc
-exec bash
 kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.0/manifests/canal.yaml
 
 join_cmd=`kubeadm token create --print-join-command`
 echo "Join command for worker nodes:"
 echo "$join_cmd --cri-socket=\"unix:///var/run/cri-dockerd.sock\""
+
+echo "export KUBECONFIG=/etc/kubernetes/admin.conf" > ~/.bashrc
+exec bash
--- production/worker-setup.sh
+++ production/worker-setup.sh
@@ -7,7 +7,9 @@ sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release << 'EOF'
+Y
+EOF
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
